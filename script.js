93e7ce01fd1e53a92885df8799d8c6bc79bc2aff
Ken <nontsuke.ken@gmail.com>
2023-12-04 14:37:36 -0500

    hamburger function complete

---
+++ script.js
@@ -0,0 +1,27 @@
+// alert("Please set Inspect mode resolution to 540 x 1170 before use")
+
+// Declaring variables for different pages
+
+// Declaring variables for different buttons inside the webpage
+const menuIcon = document.getElementById("MenuIcon")
+
+
+
+
+//Hamburger menu function----------------
+const overlay = document.getElementById("overlay");
+const transparent = document.getElementById("transparentLayer");
+
+
+//When the menuIcon is pressed menu and transparent div popup
+menuIcon.onclick = function(){
+    overlay.classList.add("show-menu");
+    transparent.style.display = "block";
+}
+
+//when user press the transparent div the menu will close along with the transparent div
+transparent.onclick = function(){
+    overlay.classList.remove("show-menu");
+    transparent.style.display = "none";
+}
+//Hamburger menu function----------------End
